22aa35a2130ef9cff2e50253defd55b86ab5d4cd
Sharafat309 <146200933+Sharafat309@users.noreply.github.com>
2023-11-01 21:01:37 +0500

    Add project files.

---
+++ Loops (Do While)/DO While.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int i = 1;
+	do {
+
+		int num = 7;
+		cout << num * i << "  ";
+		i++;
+	} while (i <= 14); cout << "\n";
+	system("pause");
+	return 0;
+}
